56475a8063ae5f10685b0da2a4abace820116e31
Dmitrii Martynov <dmitriimartynov1@gmail.com>
2020-11-27 18:47:39 +0300

    finally configured sorting styles

--- src/components/Header/DesktopSorting.jsx
+++ src/components/Header/DesktopSorting.jsx
@@ -1,6 +1,12 @@
 import { Button, ButtonGroup, makeStyles } from '@material-ui/core'
 import React from 'react'
 import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  sortName,
+  sortPrice,
+} from '../../redux/sortingAndCurrency/sortingAndCurrency.actions'
+import clsx from 'clsx'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,16 +17,98 @@ const useStyles = makeStyles((theme) => ({
       margin: theme.spacing(1),
     },
   },
+  listItemInitial: { color: 'rgba(0, 0, 0, 0.42)' },
+  listItemClicked: { backgroundColor: 'rgba(0, 0, 0, 0.08)' },
+  arrowDisabled: {
+    color: 'rgba(0, 0, 0, 0.5)',
+  },
+  iconInitial: {
+    transition: '0.7s',
+  },
+  buttonDisabled: {
+    color: 'rgba(0, 0, 0, 0.65)',
+  },
+  buttonEnabled: {
+    backgroundColor: 'rgba(0, 0, 0, 0.15)',
+  },
+  iconRotate: { transform: 'rotate(180deg)' },
 }))
 
 const DesktopSorting = () => {
   const classes = useStyles()
+  const dispatch = useDispatch()
+  const priceSortState = useSelector(
+    (state) => state.sortingAndCurrency.sorting.price
+  )
+  const nameSortState = useSelector(
+    (state) => state.sortingAndCurrency.sorting.name
+  )
+
+  const handlePriceClick = () => {
+    if (priceSortState === null) {
+      dispatch(sortPrice('Asc'))
+    } else if (priceSortState === 'Asc') {
+      dispatch(sortPrice('Desc'))
+    } else if (priceSortState === 'Desc') {
+      dispatch(sortPrice(null))
+    }
+  }
+
+  const handleNameClick = () => {
+    if (nameSortState === null) {
+      dispatch(sortName('Asc'))
+    } else if (nameSortState === 'Asc') {
+      dispatch(sortName('Desc'))
+    } else if (nameSortState === 'Desc') {
+      dispatch(sortName(null))
+    }
+  }
 
   return (
     <div className={classes.root}>
       <ButtonGroup variant='text' color='inherit' size='large'>
-        <Button startIcon={<ArrowUpwardIcon />}>Sort by Price</Button>
-        <Button endIcon={<ArrowUpwardIcon />}>Sort by Name</Button>
+        <Button
+          className={
+            priceSortState === null
+              ? classes.buttonDisabled
+              : classes.buttonEnabled
+          }
+          onClick={handlePriceClick}
+          startIcon={
+            <ArrowUpwardIcon
+              classes={{
+                colorDisabled: classes.arrowDisabled,
+              }}
+              color={priceSortState === null ? 'disabled' : 'inherit'}
+              className={clsx(classes.iconInitial, {
+                [classes.iconRotate]: priceSortState === 'Desc',
+              })}
+            />
+          }
+        >
+          Sort by Price
+        </Button>
+        <Button
+          className={
+            nameSortState === null
+              ? classes.buttonDisabled
+              : classes.buttonEnabled
+          }
+          onClick={handleNameClick}
+          endIcon={
+            <ArrowUpwardIcon
+              classes={{
+                colorDisabled: classes.arrowDisabled,
+              }}
+              color={nameSortState === null ? 'disabled' : 'inherit'}
+              className={clsx(classes.iconInitial, {
+                [classes.iconRotate]: nameSortState === 'Desc',
+              })}
+            />
+          }
+        >
+          Sort by Name
+        </Button>
       </ButtonGroup>
     </div>
   )
