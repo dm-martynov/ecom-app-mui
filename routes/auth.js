03060f46f59958072bb635fed245a63d7324ad27
Dmitrii Martynov <dmitriimartynov1@gmail.com>
2020-11-30 15:45:58 +0300

    added validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,29 @@
 const router = require('express').Router()
+const User = require('../model/User')
 
-router.post('/register', (req, res) => {
-  res.send('Register')
+const Joi = require('@hapi/joi')
+
+const schema = Joi.object({
+  name: Joi.string().min(5).required(),
+  email: Joi.string().min(5).required().email(),
+  password: Joi.string().min(6).required(),
+})
+
+router.post('/register', async (req, res) => {
+  const { error } = schema.validate(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
+
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  })
+  try {
+    const savedUser = await user.save()
+    res.send(savedUser)
+  } catch (err) {
+    res.status(400).send(err)
+  }
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3001
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 const authRoute = require('./routes/auth')
+
+dotenv.config()
+
+const port = process.env.PORT || 3001
+
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log('connected to db!')
+)
+
+app.use(express.json())
 
 app.use('/api/user', authRoute)
 
